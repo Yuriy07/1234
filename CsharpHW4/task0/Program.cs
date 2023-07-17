457a82ba20abc42ffdb1b98ea754b221d88f812e
Yuriy <>
2023-07-17 14:18:38 +0300

    HomeWork4

--- CsharpHW4/task0/Program.cs
+++ CsharpHW4/task0/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void ToDegree(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    Console.WriteLine(A + " в степени " + B + " = " + result);
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите степень B: ");
+ToDegree(numberA, numberB);
---
+++ CsharpHW4/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void Sumnum(int num, int len)
+{
+    int sum = 0;
+    for(int i = 1; i <= len; i++)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    Console.WriteLine($"Сумма цифр равен {sum}.");
+}
+
+int NumLen(int num)
+{
+    int i = 0;
+    while(num > 0)
+    {
+        num /= 10;
+        i++;
+    }
+    return i;
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = ReadInt("Введите число: ");
+int len = NumLen(num);
+Sumnum(num, len);
