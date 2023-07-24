6a6fbee4e19043370fc453a0e4f5e900cd21367e
Yuriy <>
2023-07-24 16:19:00 +0300

    HomeWork6

---
+++ CsharpHW6/task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+void Main(string[] args)
+{   
+    Console.Write("Введите количество чисел: ");
+    int M = int.Parse(Console.ReadLine());
+    int count = CountPositiveNumbers(M);
+    Console.WriteLine("Количество положительных чисел: " + count);
+}
+
+int CountPositiveNumbers(int M)
+{
+    int count = 0;
+
+    for (int i = 0; i < M; i++)
+    {
+        Console.Write("Введите число " + i + ": ");
+        double number = double.Parse(Console.ReadLine());
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Main(args);
---
+++ CsharpHW6/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void PointIntersection(double b1, double k1, double b2, double k2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = k2 * x + b2;
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+}
+
+double GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double b1 = GetInfo("Введите значение b1: ");
+double k1 = GetInfo("Введите значение k1: ");
+double b2 = GetInfo("Введите значение b2: ");
+double k2 = GetInfo("Введите значение k2: ");
+Console.WriteLine();
+PointIntersection(b1, k1, b2, k2);
